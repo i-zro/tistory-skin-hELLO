adbedade686073bc2a0da38301171bb83c15aad1
i-zro <dek3127@gmail.com>
2024-04-27 20:08:00 +0900

    actions update

---
+++ tidory.config.js
@@ -0,0 +1,79 @@
+/**
+ * Tidory Configuration
+ * https://tidory.com/docs/configuration/
+ */
+module.exports = {
+  ts_session: '',
+  url: 'process.env.URL',
+
+  preview: {
+    /**
+     * homeType
+     *
+     * NONE
+     * COVER
+     */
+    homeType: 'NONE',
+
+    /**
+     * Preview Mode
+     *
+     * index
+     * entry
+     * category
+     * tag,
+     * location
+     * media,
+     * guestbook
+     */
+    mode: 'index',
+
+    /**
+     * Skin Settings
+     */
+    skinSettings: {
+      liststyle: 'list'
+    },
+
+    /**
+     * Variables
+     */
+    variableSettings: {
+      sidebar: true,
+      'foldable-category': true,
+      'foldable-reply': true,
+      width: '720',
+      toc: true,
+      scrollspy: true,
+      hljs: 'xcode',
+      hljsDark: 'vs2015'
+    }
+  },
+
+  alias: {
+    '@': 'assets',
+    '~views': 'views'
+  },
+
+  /**
+   * Webpack Configuration
+   *
+   * @param {object} webpackConfig
+   */
+  extends (webpackConfig) {
+    webpackConfig.module.rules = [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      ...webpackConfig.module.rules
+    ]
+
+    webpackConfig.entry = {
+      vendor: './assets/vendor.js',
+      ...webpackConfig.entry
+    }
+  }
+}
